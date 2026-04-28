dc5a6dc06bda453403faf51befa2a9d04dcc760d
Gonzalo Larrica <76012483+goncho09@users.noreply.github.com>
2026-04-28 00:15:48 -0300

    feat: new profile section

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <script setup>
-import Chat from "./components/Chat.vue";
-import Chats from "./components/Chats.vue";
 import LateralBar from "./components/LateralBar.vue";
+import AppLayout from "./components/AppLayout.vue";
 </script>
 
 <template>
   <LateralBar class="flex-[0.4]" />
-  <div class="flex flex-1 min-w-0 overflow-hidden">
-    <Chats />
-    <Chat />
-  </div>
+  <AppLayout />
 </template>
---
+++ src/components/AppLayout.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import Chat from "./Chat.vue";
+import Chats from "./Chats.vue";
+import Header from "./Header.vue";
+import Profile from "./Profile.vue";
+
+import { ref } from "vue";
+
+const showProfile = ref(false);
+</script>
+
+<template>
+  <section class="flex flex-col flex-1 min-w-0 overflow-hidden">
+    <Header @toggle-profile="showProfile = !showProfile" />
+    <div class="flex flex-1 min-h-0">
+      <Chats :class="showProfile ? 'flex-[0.3]' : 'flex-[0.4]'" />
+      <Chat :class="showProfile ? 'flex-[0.4]' : 'flex-[0.6]'" />
+      <Transition name="fade">
+        <Profile v-if="showProfile" />
+      </Transition>
+    </div>
+  </section>
+</template>
