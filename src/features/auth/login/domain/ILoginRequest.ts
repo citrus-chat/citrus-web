f13b8895869208ed557d503a5e2cbc46f811939c
Zarsu <unusuariodegogle.xd@gmail.com>
2026-06-17 23:51:23 -0300

    fix

--- src/features/auth/login/domain/ILoginRequest.ts
+++ src/features/auth/login/domain/ILoginRequest.ts
@@ -3,6 +3,5 @@ import type { IDevice } from "@/features/device/domain/IDevice";
 export interface ILoginRequest {
   email: string;
   password: string;
-
-  device: IDevice;
+  deviceRequest: IDevice;
 }
